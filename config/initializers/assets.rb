9fa97605686cd310c43e9f29b200dbee22e86882
Kelsey B <kelseybcoding@gmail.com>
2016-07-06 20:36:15 -0500

    theme

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,27 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+# Rails.application.config.assets.precompile += %w( jquery-1.11.2.min.js )
+# Rails.application.config.assets.precompile += %w( jquery-ui.min.js )
+# Rails.application.config.assets.precompile += %w( jquery.easing.js )
+# Rails.application.config.assets.precompile += %w( jquery.fitvids.js )
+# Rails.application.config.assets.precompile += %w( jquery.viewportchecker.js )
+# Rails.application.config.assets.precompile += %w( jquery.stellar.min.js )
+# Rails.application.config.assets.precompile += %w( wow.min.js )
+# Rails.application.config.assets.precompile += %w( jquery.colorbox-min.js )
+# Rails.application.config.assets.precompile += %w( owl.carousel.min.js )
+# Rails.application.config.assets.precompile += %w( isotope.pkgd.min.js )
+# Rails.application.config.assets.precompile += %w( masonry.pkgd.min.js )
+# Rails.application.config.assets.precompile += %w( imagesloaded.pkgd.min.js )
+# Rails.application.config.assets.precompile += %w( jquery.fs.tipper.min.js )
+# Rails.application.config.assets.precompile += %w( mediaelement-and-player.min.js )
+# Rails.application.config.assets.precompile += %w( sidebar-menu.js )
+# Rails.application.config.assets.precompile += %w( theme.js )
+# Rails.application.config.assets.precompile += %w( navigation.js )
+
+# Rails.application.config.assets.precompile += %w( style.css )
+# Rails.application.config.assets.precompile += %w( ionicons.css )
+# Rails.application.config.assets.precompile += %w( sidebar-menu.css )
+# Rails.application.config.assets.precompile += %w( animate.css )
+# Rails.application.config.assets.precompile += %w( jquery-ui.css )
