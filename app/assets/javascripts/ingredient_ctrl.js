c3b8afdaac6dfd230c169426c3c064fa60150108
Kelsey B <kelseybcoding@gmail.com>
2016-07-11 20:14:17 -0500

    created ingredient controller

---
+++ app/assets/javascripts/ingredient_ctrl.js
@@ -0,0 +1,52 @@
+
+// (function() {
+// 	"use strict";
+
+// 	angular.module("app").controller("ingredientCtrl", function($scope,$http){
+		
+// 		$scope.setup = function() {
+// 			$http.get("/api/v1/people.json").then(function(response){
+				
+// 			$scope.people = response.data;
+// 			});
+
+// 		};
+
+// 		$scope.toggleBioVisible = function(person){
+// 			person.bioVisible = !person.bioVisible
+// 		};
+
+// 		$scope.addPerson = function(personName, personBio){
+// 				var newPerson = {
+// 					name: personName,
+// 					bio: personBio
+// 			};
+
+// 			$http.post('/api/v1/people.json', newPerson).then(function(response){
+// 				$scope.people.push(response.data);
+// 				$scope.newName = '';
+// 				$scope.newBio = '';
+// 				$scope.errors = [];
+// 			}, function(error){
+// 				$scope.errors = error.data.errors;
+
+// 			});	
+// 		};
+
+// 		$scope.deletePerson = function(index){
+// 			$scope.people.splice(index,1);
+// 		};
+
+// 		$scope.setOrderBy = function(attribute){
+
+// 			if (attribute != $scope.orderAttribute){
+// 				$scope.descending = false;
+// 			} else {
+// 				$scope.descending = !$scope.descending;
+// 			}
+
+// 			$scope.orderAttribute = attribute
+// 		};
+// 	});
+// }());
+// 	
