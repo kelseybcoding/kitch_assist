b8fc56928b28595beaefc14d45f78a4a9d79fcea
Kelsey B <kelseybcoding@gmail.com>
2016-07-16 09:07:43 -0500

    added checkboxes for pantry_items

--- app/assets/javascripts/ingredient_ctrl.js
+++ app/assets/javascripts/ingredient_ctrl.js
@@ -56,23 +56,23 @@
 		};
 
 /// UPDATE PANTRY ITEM ///
-		$scope.updatePantryItem = function(pantryItemQuantity, pantryItemUnits){
-				var changePantryItem = {
-					quantity: pantryItemQuantity,
-					units: pantryItemUnits
+		// $scope.updatePantryItem = function(pantryItemQuantity, pantryItemUnits){
+		// 		var changePantryItem = {
+		// 			quantity: pantryItemQuantity,
+		// 			units: pantryItemUnits
 
-			};
+		// 	};
 
-			$http.patch('/api/v1/pantry_items/#{pantry_item.id}.json', changePantryItem).then(function(response){
-				$scope.pantry_items.push(response.data);
-				$scope.changeQuantity = '';
-				$scope.changeUnits = '';
-				$scope.errors = [];
-			}, function(error){
-				$scope.errors = error.data.errors;
+		// 	$http.patch('/api/v1/pantry_items/#{pantry_item.id}.json', changePantryItem).then(function(response){
+		// 		$scope.pantry_items.push(response.data);
+		// 		$scope.changeQuantity = '';
+		// 		$scope.changeUnits = '';
+		// 		$scope.errors = [];
+		// 	}, function(error){
+		// 		$scope.errors = error.data.errors;
 
-			});	
-		};
+		// 	});	
+		// };
 
 
 		$scope.deletePantryItem = function(index){
